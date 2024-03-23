d87feff4122c7def53f6f264fa918272ed97daf9
Jorge M. Risco <jorgedelano7@gmail.com>
2024-03-22 22:55:53 -0300

    initial customization

--- content/index.md
+++ content/index.md
@@ -1,6 +1,7 @@
 ---
-title: Welcome to Quartz
+title: Homepage
 ---
+# Jorge M. Risco
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+
+In construction
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Jorge M. Risco 🖥️",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
